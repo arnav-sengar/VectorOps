35ba70d3fa70d283334a8e228a570d3c2084d7a9
Arnav Singh <arnavsengar1506@gmail.com>
2026-07-12 14:24:26 +0530

    Updated server.js

--- server/server.js
+++ server/server.js
@@ -7,7 +7,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/api/health', (req, res) => res.json({ status: 'ok' }));
 
+app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/vehicles', require('./routes/vehicleRoutes'));
+app.use('/api/drivers', require('./routes/driverRoutes'));
+app.use('/api/trips', require('./routes/tripRoutes'));
+app.use('/api/maintenance', require('./routes/maintenanceRoutes'));
+app.use('/api', require('./routes/fuelExpenseRoutes'));
+app.use('/api', require('./routes/reportRoutes'));
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Something went wrong' });
+});
 
 const PORT = process.env.PORT || 5000;
 
