210fc51e0876bd6d771c163a2238c3acb29335f6
Arnav Singh <arnavsengar1506@gmail.com>
2026-07-12 11:23:22 +0530

    Added MySQL config file

--- server/config/db.js
+++ server/config/db.js
@@ -1,13 +1,16 @@
-const mongoose = require('mongoose');
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log('DB connected');
-  } catch (err) {
-    console.error(err.message);
-    process.exit(1);
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 3306,
+    dialect: 'mysql',
+    logging: false, // set to console.log to see raw SQL while debugging
   }
-};
+);
 
-module.exports = connectDB;
+module.exports = sequelize;
